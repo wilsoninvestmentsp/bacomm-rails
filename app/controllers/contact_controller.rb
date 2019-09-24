a4380d53c09abda1aa76a4bfec8a06f345b6b2e6
sanjay-btc <sanjay.prajapati@botreetechnologies.com>
2019-09-24 11:52:57 +0530

    Comment zoho code to resolve app crash issue

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,7 +6,9 @@ class ContactController < ApplicationController
     p = params[:contact]
     # p.merge!(investor_source: 'Website', contact_type: 'BACOMM')
     # p.merge!(here: params[:hear_from])
-    ZohoContact.save_contact(p)
+
+    # TODO : Below code is commented temporary due to app crash issue. Something is wrong with API key
+    # ZohoContact.save_contact(p)
     flash[:success] = 'Contact created successfully!'
     redirect_to root_path
   end
